a8ca10736245fd1cb8bb32b064678c61f23fe329
DanyGodinhoBadich <dany.godinho.badich@gmail.com>
2025-05-28 21:48:14 +0200

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -6,7 +6,8 @@ $string = "This is a test, hope it works!"
 # Met -split worden de letters appart gehouden in een array.
 $arr = $string -split ""
 $arr
-# 
+# De letters worden van plaats verwisselt.
 [array]::Reverse($arr)
 $arr
+# De Omgekeerde letters worden samengevoegd in $arr en vormen een omgekeerde zin.
 $arr -join ''
